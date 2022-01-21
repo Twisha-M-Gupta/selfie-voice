9cf13ed36fd5399b5daa84d7446b8357d17f1749
Twisha-M-Gupta <90875017+Twisha-M-Gupta@users.noreply.github.com>
2022-01-20 20:07:54 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,23 @@ recognition.onresult = function(event) {
     console.log(content);
 
     document.getElementById("voiceToText").innerHTML = content;
+    speak();
 }
+
+
+function speak() {
+    var synth = window.speechSynthesis;
+    var speechBack = document.getElementById("voiceToText").value;
+    var utterThis = new SpeechSynthesisUtterance(speechBack);
+    synth.speak(utterThis);
+    Webcam.attach(camera1);
+}
+
+camera1 = document.getElementById("camera");
+
+Webcam.set({
+    width: 500,
+    height: 350,
+    image_format: 'png',
+    png_quality: 95
+});
